963dcd4b1b7ac5c00bb158cd3daf39984cea2348
Ouzman <ouz001@csiro.au>
2020-04-23 17:55:48 +1000

    Hennsey problem fixed

--- Landmark/Andrew_McManhen/NOC2_April.R
+++ Landmark/Andrew_McManhen/NOC2_April.R
@@ -869,8 +869,8 @@ zone_av_1_rate3vsGR_res_sig
  
  
  site <- filter(harm_database,
-                Paddock_tested == "Hennessy_loam") %>% 
-                #Paddock_tested == Paddock_tested_db) %>%  
+                
+                Paddock_tested == Paddock_tested_db) %>%  
                 #Paddock_tested == Paddock_tested_db &
                 #  Farmer == Farmer_db) %>% 
    dplyr::select(5, 6: 11)
