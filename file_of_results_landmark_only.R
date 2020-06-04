b5a75e6d7486f6f63840994fdc13f37d968c6f77
Ouzman <ouz001@csiro.au>
2020-06-04 11:16:33 +1000

    complied results for TM

--- file_of_results_landmark_only.R
+++ file_of_results_landmark_only.R
@@ -2,7 +2,7 @@
 
 #1. install the lib that I will be using for this work
 
-libs <- c("dplyr", "tidyr", 
+libs <- c("dplyr", "tidyverse", "stringr",
           "ggplot2", "readxl",
           "PairedData", "cowplot", "grid", 
           "RGraphics", 
@@ -24,94 +24,99 @@ load.libraries(libs)
 
 #bring in th zone analysis results for each site.
 #what are the files that have been finished?
-
+getwd()
 path_finished_wk <- "W:/value_soil_testing_prj/Yield_data/finished/"
+#path_finished_wk <- "C:/Users/ouz001/Desktop/soil_testing_proj_full/finished/"
 setwd(path_finished_wk)
 
-file_names1 <- list.files(path = path_finished_wk,
-                          pattern = ".csv") 
-print(file_names1)
+my_list_landmark_files <- list.files(path = path_finished_wk,
+                                     pattern = "^[Landmark]")
+#pattern = ".csv") 
+print(my_list_landmark_files)
+#remove the summary landmark file Landmark_15_05_2020.csv - which appear first
+landmark_files <- my_list_landmark_files[2:40]
+landmark_files
 
-################################
-#now which ones do I want to merge?
-
-
-org_contact_farmer <- "Landmark_Kris_Dixon_6_Heightons_1"
-
-####Some files are missing paddock info so I need to add these now - oops!
-file_names2 <- list.files(path = path_finished_wk,
-                          pattern=glob2rx(paste0("*",org_contact_farmer,"*", ".csv")))
-
-print(file_names2)
-
-modify <- read.csv("Landmark_Steve_Richmond_5_Clarke_Bros_1_07_Back_Grussing__zones.csv")
-modify <- modify %>% 
-  mutate(Paddock_tested = "07_Back_Grussing")
-write_csv(modify,"Landmark_Steve_Richmond_5_Clarke_Bros_1_07_Back_Grussing__zones.csv")
-
-
-###This is for when we have paddock and zone in the data analysis file.
-#########################################################################################################
-
-
-file_names2 <-
-  c(
-  # "Landmark_Steve_Richmond_5_AC_Jacka_2_Heads__zones.csv")#,
-  #   "Landmark_Kris_Dixon_6_Heightons_1_McKenzie__zones.csv",
-  #   "Landmark_Kris_Dixon_6_Heightons_1_Bellas__zones.csv")#,
-  #   "Landmark_Tom_Cooper_9_A_P_Robertson_1_McPhees__zones.csv",
-  #   "Landmark_Tom_Cooper_9_A_P_Robertson_1_Bird__zones.csv",
-  #   "Landmark_Tom_Cooper_9_A_P_Robertson_1_Affecks__zones.csv"),
-  #   "Landmark_Matt_Nihill_7_GRG_Weeks_1_Reids_mount__zones.csv",
-  #   "Landmark_Matt_Nihill_7_GRG_Weeks_1_Reids_mid__zones.csv")#,
-  #   "Landmark_Steve_Richmond_5_Clarke_Bros_1_30_below_house__zones.csv",
-      "Landmark_Steve_Richmond_5_Clarke_Bros_1_07_Back_Grussing__zones.csv")
-  # )
-
-
-# file_names2 <- list.files(path = path_finished_wk,
-#                           pattern=glob2rx(paste0("*",org_contact_farmer,"*", ".csv")))
-print(file_names2)
-# read in each .csv file in file_list and rbind them into a data frame called data 
 data <- 
   do.call("rbind", 
-          lapply(file_names2, 
+          lapply(landmark_files, #this is a list of files
                  function(x) 
                    read.csv(paste(path_finished_wk, x, sep=''), 
                             stringsAsFactors = FALSE)))
 
-###########################################################################################################
-Landmark_Steve_Richmond_5_AC_Jacka_2_Heads <- data
-Landmark_Kris_Dixon_6_Heightons_1 <- data
-Landmark_Tom_Cooper_9_A_P_Robertson_1 <- data
-Landmark_Matt_Nihill_7_GRG_Weeks_1 <- data
-Landmark_Steve_Richmond_5_Clarke_Bros_1 <- data
-#new set that I was missing
-Landmark_Steve_Richmond_5_Clarke_Bros_1_07_Back_Grussing <- data
-
-### Bring in all that is run into one df
-
-dim(Landmark_Steve_Richmond_5_AC_Jacka_2_Heads)#16
-dim(Landmark_Kris_Dixon_6_Heightons_1)#16
-dim(Landmark_Tom_Cooper_9_A_P_Robertson_1)#16
-dim(Landmark_Matt_Nihill_7_GRG_Weeks_1)#16
-dim(Landmark_Steve_Richmond_5_Clarke_Bros_1)#16
 
 
-results_15_05_2020 <- rbind(Landmark_Steve_Richmond_5_AC_Jacka_2_Heads, 
-                            Landmark_Kris_Dixon_6_Heightons_1,
-                            Landmark_Tom_Cooper_9_A_P_Robertson_1,
-                            Landmark_Matt_Nihill_7_GRG_Weeks_1,
-                            Landmark_Steve_Richmond_5_Clarke_Bros_1)
+
+# ####Some files are missing paddock info so I need to add these now - oops!
+# modify <- read.csv("Landmark_Steve_Richmond_5_AC_Jacka_2_Home_6__zones.csv")
+# modify <- modify %>% 
+#   mutate(Paddock_tested = "Home_6")
+# write_csv(modify,"Landmark_Steve_Richmond_5_AC_Jacka_2_Home_6__zones.csv")
+# 
+# ####Some files are missing paddock info and other info so I need to add these now - oops!
+# modify <- read.csv("Landmark_James_Falvey_2_Tim_McClelland_4_Backsheehans__zones.csv")
+# modify <- modify %>% 
+#   mutate(Details = "not supplied yet",
+#          Starter_Feriliser = "not supplied yet",
+#          Topdress = "not supplied yet",
+#          Zone = "no zones")
+# write_csv(modify,"Landmark_James_Falvey_2_Tim_McClelland_4_Backsheehans__zones.csv")
 
 
-write.csv(results_15_05_2020, "Landmark_15_05_2020.csv")
+##these are the files that have clm added to them
+dim(read.csv( "Landmark_Claire_Gutsche_6_Lister_Trading_1_Bevs__zones.csv" ))  #16     
+dim(read.csv( "Landmark_Claire_Gutsche_6_Lister_Trading_1_Willings_Willows__zones.csv"))#16
+dim(read.csv( "Landmark_Hamish_Verco_7_Ballinger_1_Reschkes_North__zones.csv"))  #16       
+dim(read.csv( "Landmark_Heath_Griffith_5_Campbell_2_Brennans__zones.csv"))   #16       
+dim(read.csv( "Landmark_James_Falvey_2_Tim_McClelland_4_Clover__zones.csv"))  #16          
+dim(read.csv("Landmark_James_Falvey_2_Tim_McClelland_4_Hennessy__zones.csv"  )) #16  
+dim(read.csv( "Landmark_James_Falvey_2_Tim_McClelland_4_Top__zones.csv"  ))  #16 
+dim(read.csv( "Landmark_James_Falvey_2_Tim_McClelland_4_Mervyns__strip.csv" )) #16 
+dim(read.csv( "Landmark_James_Falvey_2_Tim_McClelland_4_Backsheehans__zones.csv" )) #16
 
-#oops I had some missing data
-results_15_05_2020 <- read.csv("Landmark_15_05_2020.csv")
-results_15_05_2020 <- dplyr::select(results_15_05_2020, - "X.1")
-results_15_05_2020 <- rbind(results_15_05_2020,
-                            Landmark_Steve_Richmond_5_Clarke_Bros_1_07_Back_Grussing) 
+#these were good from the start
+dim(read.csv("Landmark_Alister_Tippert_1_Chris_Shark_4_Balls_Centre_2__zones.csv")) #16
+dim(read.csv("Landmark_Alister_Tippert_1_Chris_Shark_4_Balls_Centre_2__zones.csv" ))   
+dim(read.csv("Landmark_Alister_Tippert_1_Chris_Shark_4_Balls_Silo_1__zones.csv" ))     
+dim(read.csv("Landmark_Alister_Tippert_1_Leighview_2_Jash_5__zones.csv" ))             
+dim(read.csv( "Landmark_Alister_Tippert_1_PaulJenz_3_Harrys_Long_2__zones.csv"))        
+dim(read.csv( "Landmark_Andrew_McMahen_8_Barney_OCallaghan_2_Hickmont__zones.csv" ))    
+dim(read.csv( "Landmark_Andrew_McMahen_8_Barney_OCallaghan_2_Jardines__zones.csv" ))    
+dim(read.csv( "Landmark_Andrew_McMahen_8_Barney_OCallaghan_2_NOC_2__zones.csv" ))       
+dim(read.csv( "Landmark_Andrew_McMahen_8_Birkinshaw_1_Rail__zones.csv"   ))             
+dim(read.csv( "Landmark_Andrew_McMahen_8_Birkinshaw_1_Round_Home__zones.csv"))          
+dim(read.csv( "Landmark_Chris_Dunn_7_Jenharwil_2_Brooklands_2__zones.csv"  ))           
+dim(read.csv( "Landmark_Chris_Dunn_7_Jenharwil_2_James_2__zones.csv" ))                 
+dim(read.csv( "Landmark_Chris_Dunn_7_Jenharwil_2_Lindsey_Cali__zones.csv"  )) 
+dim(read.csv( "Landmark_Claire_Gutsche_6_Lister_Trading_1_East_Laundys__zones.csv" ))#16 
+dim(read.csv( "Landmark_James_Falvey_2_David_Ferrier_School__zones.csv"))               
+dim(read.csv( "Landmark_James_Falvey_2_Frankel_2_Front_1__zones.csv"  ))                
+dim(read.csv( "Landmark_James_Falvey_2_Frankel_2_Front_3__zones.csv"  ))                
+dim(read.csv( "Landmark_James_Falvey_2_Frankel_2_Stewarts_3__zones.csv"  ))             
+dim(read.csv( "Landmark_James_Falvey_2_Lehmann_3_Back__zones.csv"   ))                  
+dim(read.csv( "Landmark_James_Falvey_2_Lehmann_3_Swamp__zones.csv" ))                   
+dim(read.csv( "Landmark_James_Falvey_2_Tim_McClelland_4_Backsheehans__zones.csv"  ))  
+dim(read.csv( "Landmark_Kris_Dixon_6_Heightons_1_Bellas__zones.csv"  ))                 
+dim(read.csv( "Landmark_Kris_Dixon_6_Heightons_1_McKenzie__zones.csv"  ))               
+dim(read.csv( "Landmark_Matt_Nihill_7_GRG_Weeks_1_Reids_mid__zones.csv"   ))            
+dim(read.csv( "Landmark_Matt_Nihill_7_GRG_Weeks_1_Reids_mount__zones.csv"  ))           
+dim(read.csv( "Landmark_Steve_Richmond_5_AC_Jacka_2_Heads__zones.csv"   ))              
+dim(read.csv( "Landmark_Steve_Richmond_5_AC_Jacka_2_Home_6__zones.csv"  ))              
+dim(read.csv( "Landmark_Steve_Richmond_5_Clarke_Bros_1_07_Back_Grussing__zones.csv"  )) #16
+dim(read.csv( "Landmark_Steve_Richmond_5_Clarke_Bros_1_30_below_house__zones.csv"  ))   
+dim(read.csv( "Landmark_Tom_Cooper_9_A_P_Robertson_1_Affecks__zones.csv" ))             
+dim(read.csv( "Landmark_Tom_Cooper_9_A_P_Robertson_1_Bird__zones.csv"))                 
+dim(read.csv( "Landmark_Tom_Cooper_9_A_P_Robertson_1_McPhees__zones.csv"))   
 
-str(results_15_05_2020)
+          
+
+
+write.csv(data, paste0(path_finished_wk,
+                                      "complied",
+                                      "/",
+                                      "Landmark_",
+                                      Sys.Date(),
+                                      "_For_TM.csv"))
+    
+
 
