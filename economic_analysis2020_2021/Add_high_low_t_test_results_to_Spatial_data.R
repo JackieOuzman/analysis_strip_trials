ecb01fce4cb0ceaf3804642e113a703d23d102cd
Jackie Ouzman <jackie.ouzman@csiro.au>
2021-02-17 13:13:46 +1030

    adding in ricks tables

--- economic_analysis2020_2021/Add_high_low_t_test_results_to_Spatial_data.R
+++ economic_analysis2020_2021/Add_high_low_t_test_results_to_Spatial_data.R
@@ -44,16 +44,32 @@ join <- left_join(t_test_results, spatial_data_no_yld, by= c("paddock_ID" = "Pad
 
 
 
-write.csv(join, "W:/value_soil_testing_prj/Yield_data/analysis_strip_trials_April/economic_analysis2020_2021/results_for_table_high_low.csv")
+#write.csv(join, "W:/value_soil_testing_prj/Yield_data/analysis_strip_trials_April/economic_analysis2020_2021/results_for_table_high_low.csv")
 str(join)
 
 ### ricks table
 
-## for P trials count
+## for P trials count to make a table
 high_low_P_N_trials <- join %>% 
   group_by(Strip_Type, comparison, yld_response) %>% 
   summarise(count = n())
   
+str(join)
 
-high_low_P_N_trials
-          
+join$comparison <- factor(join$comparison, levels = c("high_v_low", "high_v_medium", "medium_v_low"), 
+                  labels = c("high fert rate vs low fert rate", "high fert rate vs medium fert rate", "medium fert rate vs low fert rate"))
+
+
+  ggplot(join, mapping = aes(x=yld_response, fill = Significant))+
+  geom_bar() +
+  facet_grid(Strip_Type ~ comparison)+
+  theme_bw()+
+  scale_x_discrete(labels = c("positive" = "positive",
+                              "no_response" = "no response",
+                              "negative" = "negative"))+
+  geom_text(aes(y = ((..count..)), label = ((..count..))), stat = "count", vjust = -0.25) +
+  labs(title = "Comaprsion of yield respsonse in strips with different fertiliser rate (per paddocks and zone)", 
+       subtitle = "The yield response is classed as positive when higher rates of fertiliser result in higher yields",
+       y = "Count of paddocks in yield response class", 
+       x = "yield response")
+
