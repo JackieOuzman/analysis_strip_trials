d8b68725d623bfcfcd09d79d6ef3d39111f41923
Jackie Ouzman <jackie.ouzman@csiro.au>
2021-03-11 14:32:49 +1030

    comapre different methods

--- check_of_outputs/t-test Chcek.R
+++ check_of_outputs/t-test Chcek.R
@@ -1,7 +1,7 @@
-### check on t test for jampot east zones
+### check on t test for jampot east zones 2021
 
-zone_x <-  2
-rate_x <- 2
+zone_x <-  1
+rate_x <- 1
 #function_paired_ttest <- function(strips, zone_x, rate_x){
   
   #select the zone data and the rates
@@ -28,48 +28,70 @@ rate_x <- 2
   list_SegmentID_values <- zone_x_rateXvsGR_av$SegmentID[duplicated(zone_x_rateXvsGR_av$SegmentID)] #this returns a list of values I want to keep
   zone_x_rateXvsGR_av <- zone_x_rateXvsGR_av %>% filter(SegmentID %in% list_SegmentID_values)
   
-  write.csv(zone_x_rateXvsGR_av, "W:/value_soil_testing_prj/Yield_data/2020/processing/r_scripts/check_of_outputs/jampot_east_input_t_test.csv")
+  #write.csv(zone_x_rateXvsGR_av, "W:/value_soil_testing_prj/Yield_data/2020/processing/r_scripts/check_of_outputs/jampot_east_input_t_test.csv")
   
   
   zone_x_rateXvsGR_av
   # run paired ttest
-  zone_x_rateXvsGR_res <- t.test(YldMassDry ~ Rate, data = zone_x_rateXvsGR_av, paired = TRUE)
- 
+  zone_x_rateXvsGR_res <- t.test(YldMassDry ~ Rate, data = zone_x_rateXvsGR_av, 
+                                 paired = TRUE)
+  zone_x_rateXvsGR_res
+  
+  
+  ###############################################
   DF_1_rate50_zone2 <- zone_x_rateXvsGR_av %>% 
     filter(Rate == 50)
   DF_2_rate100_zone2 <- zone_x_rateXvsGR_av %>% 
     dplyr::filter(Rate == 50)
-  DF_1_rate50_zone2 <- ungroup(DF_1_rate50_zone2)
-  DF_2_rate100_zone2 <- ungroup(DF_2_rate100_zone2)
-  dim(DF_1_rate50_zone2)
-  dim(DF_2_rate100_zone2)
-  
-  
-  zone_x_rateXvsGR_res_alt <- t.test(DF_1_rate50_zone2, DF_2_rate100_zone2, var.equal = TRUE, paired = TRUE)
-  t.test(DF_1_rate50_zone2, DF_2_rate100_zone2 )
-  
-  
-  zone_x_rateXvsGR_res
-  
-  
-  #####test results
-  # Report values from the t.test
-  zone_x_rateXvsGR_res_sig <-
-    data.frame(P_value = as.double(zone_x_rateXvsGR_res$p.value),
-               Mean_diff = (zone_x_rateXvsGR_res$estimate)) %>%
-    mutate(
-      rate_name = paste0("rate", rate_x),
-      zone = paste0("zone", zone_x),
-      rounded = abs(round(Mean_diff, 2)),
-      Significant = case_when(P_value < 0.05 ~ "significant",
-                              TRUE ~ "not significant"))
-  zone_x_rateXvsGR_res_sig 
+  #DF_1_rate50_zone2 <- ungroup(DF_1_rate50_zone2)
+  #DF_2_rate100_zone2 <- ungroup(DF_2_rate100_zone2)
+  dim(zone_x_rateXvsGR_av) #22 points
+  dim(DF_1_rate50_zone2)   #10 points
+  dim(DF_2_rate100_zone2)  #10 points
   
   
   
-  return(data.frame(zone_x_rateXvsGR_res_sig))
-}
-assign(paste0("zone_", "1", "rate_", "1"), function_paired_ttest(strips, 1, 1))
-assign(paste0("zone_", "1", "rate_", "2"), function_paired_ttest(strips, 1, 2))
-assign(paste0("zone_", "2", "rate_", "1"), function_paired_ttest(strips, 2, 1))
-assign(paste0("zone_", "2", "rate_", "2"), function_paired_ttest(strips, 2, 2))
+  ### Different options ...
+ t_test_paired <- t.test(YldMassDry ~ Rate, 
+                                        data = zone_x_rateXvsGR_av,
+                                        paired = TRUE,
+                                        var.equal = FALSE)
+ t_test_paired # p-value = 0.02802 var.equal can be set to TRUE or FALSE and produces same results
+  
+ t_test_Not_paired_var_equal_T <- t.test(YldMassDry ~ Rate, 
+                                            data = zone_x_rateXvsGR_av,
+                                        paired = FALSE,
+                                        var.equal = TRUE)
+ t_test_Not_paired_var_equal_T # p-value = 0.4002
+  
+ t_test_Not_paired_var_equal_F <- t.test(YldMassDry ~ Rate, 
+                                           data = zone_x_rateXvsGR_av,
+                                           paired = FALSE,
+                                           var.equal = FALSE)
+ t_test_Not_paired_var_equal_F #p-value = 0.4009
+  
+  
+  comapre_t_test <-
+    data.frame(t_test_paired = 
+                 as.double(t_test_paired$p.value),
+               
+               t_test_Not_paired_var_equal_T = 
+                 as.double(t_test_Not_paired_var_equal_T$p.value),
+               
+               t_test_Not_paired_var_equal_F = 
+                 as.double(t_test_Not_paired_var_equal_F$p.value))
+  
+  comapre_t_test <- pivot_longer(comapre_t_test, 
+                                 cols = starts_with("t_test"),
+                                 names_to = "P_test",
+                                 values_to = "P_value",)
+  
+  comapre_t_test <- mutate(comapre_t_test,
+                           Significant = case_when(
+                             P_value < 0.05 ~ "significant",
+                            TRUE ~ "not significant"))
+  comapre_t_test                     
+               
+     
+  
+  
